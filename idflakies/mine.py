9c25a83e5ff7522c404b3547d364348eb225afa1
Gustavo Pinto <gustavohenrique.86@gmail.com>
2019-11-21 22:32:46 -0300

    fixing a bug when exist more than one test with the same name

--- idflakies/mine.py
+++ idflakies/mine.py
@@ -3,6 +3,8 @@ import csv
 import os
 import shutil
 import tempfile
+import sys
+import re
 
 def main():
     basedir = os.getcwd()
@@ -26,22 +28,25 @@ def main():
                 if (not dir is None):
                     os.chdir("..")
                     if (os.path.exists(dir)):
-                        shutil.rmtree(dir, ignore_errors=True)
-                # get the name of the project 
+                        pass
+                        #shutil.rmtree(dir, ignore_errors=True)
+                # get the name of the project
                 parts = url.split("/")
                 dir=parts[len(parts)-1]
                 if dir == None:
                     raise Exception("fatal error")
                 # delete corresponding directory if, for some reason, a dir with this name exists
                 if (os.path.exists(dir)):
-                    shutil.rmtree(dir, ignore_errors=True)
+                    #shutil.rmtree(dir, ignore_errors=True)
+                    pass
                 # clone the repository
-                print("cloning project " + url)
-                myprocess = subprocess.Popen(['git', 'clone', url], 
-                            stdout=subprocess.PIPE, 
-                            stderr=subprocess.STDOUT)
-                stdout,stderr = myprocess.communicate()    
-                # change directory to this project
+                if (not os.path.exists(dir)):
+                    print("cloning project " + url)
+                    myprocess = subprocess.Popen(['git', 'clone', url],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
+                    stdout,stderr = myprocess.communicate()
+                    # change directory to this project
                 os.chdir(dir)
                 # add url to the list of urls
                 urls.add(url)
@@ -55,16 +60,28 @@ def main():
             testfile = parts[len(parts)-2]
             print(' processing file {}:{}'.format(sha, testname))
             # find location where the test file is
-            myprocess = subprocess.Popen(['find', '.', '-name', "{}.java".format(testfile)], 
-                        stdout=subprocess.PIPE, 
-                        stderr=subprocess.STDOUT)
-            stdout,stderr = myprocess.communicate()  
-            # download file at a given revision
-            path_to_testfile = stdout.decode("utf-8").strip(" \n")
-            myprocess = subprocess.Popen(['git', 'show', "{}:{}".format(sha, path_to_testfile)], 
-                        stdout=subprocess.PIPE, 
+            myprocess = subprocess.Popen(['find', '.', '-name', "{}.java".format(testfile)],
+                        stdout=subprocess.PIPE,
                         stderr=subprocess.STDOUT)
             stdout,stderr = myprocess.communicate()
+            # download file at a given revision
+
+            result = stdout.decode("utf-8")
+
+            if(len(re.findall('un', result)) == 1):
+                path_to_testfile = result.strip(" \n")
+            else:
+                path_to_testfile = result.split("\n")[0]
+
+            myprocess = subprocess.Popen(['git', 'show', "{}:{}".format(sha, path_to_testfile)],
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
+            stdout,stderr = myprocess.communicate()
+
+            if (b"exists on disk, but not in" in stdout):
+                print("git show {}:{}".format(sha,testcase))
+                continue
+
             if (b"fatal" in stdout):
                 print("could not locate file for {}:{}:{}".format(dir,testcase,sha))
                 continue
@@ -75,13 +92,14 @@ def main():
                 tmp.write(stdout.decode("utf-8"))
 
             # extract actual test from files
-            myprocess = subprocess.Popen(['java', '-jar', basedir+'/vis_method/build/libs/vis_method.jar', tmpfile, testname], 
-                    stdout=subprocess.PIPE, 
+            myprocess = subprocess.Popen(['java', '-jar', basedir+'/vis_method/build/libs/vis_method.jar', tmpfile, testname],
+                    stdout=subprocess.PIPE,
                     stderr=subprocess.STDOUT)
             mbody,stderr = myprocess.communicate()
             # create a file with the test case
             with open(basedir+"/tests/"+testcase, "wt+") as testfile:
                 testfile.write(mbody.decode("utf-8"))
+
 
 if __name__ == "__main__":
     main()
